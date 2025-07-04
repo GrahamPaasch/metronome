e80941a706cc774b69dca1fcf57c0ab79a1899f4
Graham Paasch <grahampaasch@gmail.com>
2025-07-04 15:31:09 -0500

    Revert "Restore full-featured Musical Practice Companion with simple sheet music transcription"
    This reverts commit 3270951a7d118b9f7556794074c91f08b56073ed.

--- musical-practice-companion/src/agents/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Agent exports for easy importing
- */
-
-export { BaseAgent } from './BaseAgent';
-export { TempoAgent } from './TempoAgent';
-export { AudioAgent } from './AudioAgent';
-export { HarmonyAgent } from './HarmonyAgent';
